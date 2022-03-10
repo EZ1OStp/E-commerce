186d47caea954aa6f2bb67010ca97e2a5d80780f
EZ1OStp <hery.stephane.rarivoson@gmail.com>
2022-03-10 13:58:23 +0300

    creation des tables

--- database/migrations/2022_03_10_090813_create_produits_table.php
+++ database/migrations/2022_03_10_090813_create_produits_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('produits', function (Blueprint $table) {
             $table->id();
-            $table->string('lastname');
-            $table->string('firstname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->double('price');
+            $table->integer('quatity');
+            $table->text('description');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -33,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('produits');
     }
 };
